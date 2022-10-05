94c520192a36b16f7c688b1645bf42e44f8b440b
kallyane-nascimento16 <110573013+kallyane-nascimento16@users.noreply.github.com>
2022-10-05 17:23:51 -0300

    Add files via upload

---
+++ parametross/parametros/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parametros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            soma objb = new soma();
+            Console.WriteLine(objb.letras());
+            Console.WriteLine("digite o primeiro numero ");
+            objb.numero1 = int.Parse (Console.ReadLine());
+            Console.WriteLine("digite o segundo numero ");
+            objb.numero2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("digite o terceiro numero ");
+            objb.numero3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("resultado da soma dos tres numeros : " + objb.somaa (objb.numero1 , objb.numero2,objb.numero3));
+
+
+
+
+        }
+    }
+}
---
+++ parametross/parametros/soma.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parametros
+{
+    internal class soma
+    {
+        public int  numero1,numero2, numero3;
+
+       internal string letras()
+        {
+            return " boa noite ";
+        } 
+        internal int somaa(int numero1 , int numero2 ,int numero3)
+        {
+            return numero1 + numero2 + numero3;
+        }
+    }
+}
